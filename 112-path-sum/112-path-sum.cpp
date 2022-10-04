c87e0b25f50726424b5ba778cadaf3c0e42d8d4f
katurivinay <85672552+katurivinay@users.noreply.github.com>
2022-10-04 16:29:00 +0530

    Time: 20 ms (48.60%), Space: 21.3 MB (74.14%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -12,13 +12,17 @@
 class Solution {
 public:
     bool helper(TreeNode* root, int targetSum,int sum){
-        if(root == NULL)return false;
-        if(root->left == NULL and root->right == NULL){
+        if(root == NULL)return false;   // base cases4
+        
+        /** do something before calling ***/
+        if(root->left == NULL and root->right == NULL){  // break case 
             if(sum + root->val == targetSum)return true;
         }
-        bool lt = helper(root->left,targetSum,sum + root->val);
-        bool rt = helper(root->right,targetSum,sum + root->val);
-        if(lt || rt)return true;
+        
+        bool lt = helper(root->left,targetSum,sum + root->val); // calling left tree
+        bool rt = helper(root->right,targetSum,sum + root->val);//calling right tree
+        
+        if(lt || rt)return true; //do something after obtainig left and right answers
         return false;
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
