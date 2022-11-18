667ac0cea86e7ff8f059b74d45712f8e44e97ea5
katurivinay <85672552+katurivinay@users.noreply.github.com>
2022-11-18 19:39:45 +0530

    Time: 2 ms (67.09%), Space: 5.9 MB (69.56%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        for (auto factor : {2, 3, 5}) {
+            n = keepDividingWhenDivisible(n, factor);
+        }
+        return n == 1;
+    }
+    int keepDividingWhenDivisible(int dividend, int divisor) {
+        while (dividend % divisor == 0) {
+            dividend /= divisor;;
+        }
+        return dividend;
+    }
+};
