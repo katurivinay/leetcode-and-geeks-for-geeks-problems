cf5d9ae7863c0560f00fd44e2e4313d0bf2276f6
katurivinay <85672552+katurivinay@users.noreply.github.com>
2023-01-21 12:35:13 +0530

    Added solution - LeetHub

--- Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.cpp
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.cpp
@@ -8,41 +8,42 @@ class Solution{
     public:
     int lenOfLongSubarr(int A[],  int n, int k) 
     { 
-    //     int sum = 0;
-    //   unordered_map<int,int> mp;
-    //   int ans = 0;
-    //   for(int i = 0 ; i< n ; i++){
-    //       sum = sum + A[i];
-    //       if(sum==k)
+        int sum = 0;
+      unordered_map<int,int> mp;
+      int ans = 0;
+      for(int i = 0 ; i< n ; i++){
+          sum = sum + A[i];
+          if(sum==k)
+            {
+                ans = max(ans,i+1);
+            }
+          else if(mp.find(sum - k) != mp.end()){
+              ans = max(ans, i - mp[sum - k]);
+          }
+          if(mp.count(sum) == 0)
+          mp[sum] = i; 
+      }
+      return ans;
+    //  int sum = 0;
+    //     int maxi = 0;
+    //     unordered_map<int,int> mp;
+    //     for(int i = 0;i<n;i++)
+    //     {
+    //         sum += A[i];
+    //         if(sum==k)
     //         {
-    //             ans = max(ans,i+1);
+    //             maxi = max(maxi,i+1);
     //         }
-    //       else if(mp.find(sum - k) != mp.end()){
-    //           ans = max(ans, i - mp[sum - k]);
-    //       }
-    //       mp[sum] = i; 
-    //   }
-    //   return ans;
-     int sum = 0;
-        int maxi = 0;
-        unordered_map<int,int> mp;
-        for(int i = 0;i<n;i++)
-        {
-            sum += A[i];
-            if(sum==k)
-            {
-                maxi = max(maxi,i+1);
-            }
-            else if(mp.count(sum-k)>0)
-            {
-                maxi = max(maxi,i - mp[sum-k]);
-            }
-            if(mp.count(sum)==0)
-            {
-              mp[sum] = i;
-            }
-        }
-        return maxi;
+    //         else if(mp.count(sum-k)>0)
+    //         {
+    //             maxi = max(maxi,i - mp[sum-k]);
+    //         }
+    //         if(mp.count(sum)==0)
+    //         {
+    //           mp[sum] = i;
+    //         }
+    //     }
+    //     return maxi;
     } 
 
 };
