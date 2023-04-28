ef8f93b9e617dff717932b49f7d92ad8ed8def46
katurivinay <85672552+katurivinay@users.noreply.github.com>
2023-04-28 17:49:10 +0530

    Time: 264 ms (56.84%), Space: 96.2 MB (76.68%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(),nums.end());
         int n = nums.size();
         int l = 0 , r = n-1,ans = 0;
-        while(l <= r){
+        while(l < r){
             ans = max(ans , nums[l] + nums[r]);
             l++;r--;
         }
