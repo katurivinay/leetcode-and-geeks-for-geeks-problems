ebedd259d62cb717e3e25971d4ac64abefd094cb
katurivinay <85672552+katurivinay@users.noreply.github.com>
2023-05-02 12:15:41 +0530

    Time: 6 ms (92.06%), Space: 8.2 MB (83.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string str) {
-        if(str.size()==0)
-      return 0;
-  int maxans = INT_MIN;
-  unordered_set < int > set;
-  int l = 0;
-  for (int r = 0; r < str.length(); r++) // outer loop for traversing the string
-  {
-    if (set.find(str[r]) != set.end()) //if duplicate element is found
-    {
-      while (l < r && set.find(str[r]) != set.end()) {
-        set.erase(str[l]);
-        l++;
+    int lengthOfLongestSubstring(string s) {
+        vector < int > mpp(256, -1);
+
+      int left = 0, right = 0;
+      int n = s.size();
+      int len = 0;
+      while (right < n) {
+        if (mpp[s[right]] != -1)
+          left = max(mpp[s[right]] + 1, left);
+
+        mpp[s[right]] = right;
+
+        len = max(len, right - left + 1);
+        right++;
       }
-    }
-    set.insert(str[r]);
-    maxans = max(maxans, r - l + 1);
-  }
-  return maxans;
+      return len;
     }
 };
