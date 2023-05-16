18f2a00edf95fa2f84039a87d975be6a424f42a6
katurivinay <85672552+katurivinay@users.noreply.github.com>
2023-05-16 11:01:36 +0530

    Time: 153 ms (31.50%), Space: 93 MB (51.07%) - LeetHub

---
+++ 2681-power-of-heroes/2681-power-of-heroes.cpp
@@ -0,0 +1,16 @@
+#define ll long long
+int M = 1000000007;
+class Solution {
+public:
+    int sumOfPower(vector<int>& nums) {
+        ll n = nums.size();
+        sort(nums.begin() , nums.end());
+        
+        ll ans = 0 , sum = 0;
+        for(int i = 0 ; i < n ; i++){
+            ans = (ans +  (sum + nums[i])* nums[i] % M * nums[i] % M ) % M;
+            sum = (sum * 2 + nums[i] ) % M;
+        }
+        return ans;
+    }
+};
